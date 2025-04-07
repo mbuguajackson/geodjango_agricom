ef81652bd78c2ae9f95927e33c235b4ca7a9c19d
mbuguajackson <jak.mbugua@gmail.com>
2025-04-07 06:58:45 +0300

    geomodels

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import Incidence
+from leaflet.admin import LeafletGeoAdmin
 # Register your models here.
+class IncidenceAdmin(LeafletGeoAdmin):
+    list_display=('name','location')
+
+admin.site.register(Incidence, IncidenceAdmin)
--- reporter/models.py
+++ reporter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.gis.db import models
 # Create your models here.
+
+class Incidence(models.Model):
+    name=models.CharField(max_length=20)
+    location=models.PointField(srid=4326)
+
+    def __str__(self):
+        return self.name
