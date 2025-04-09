c82f7ae5831711f42d8c43dbb0b8d48fdd536acd
mbuguajackson <jak.mbugua@gmail.com>
2025-04-09 11:23:19 +0300

    requirements

--- agricom/settings.py
+++ agricom/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'poolmode': 'transaction',
+  
     }
 }
 
